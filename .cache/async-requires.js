99c555d2bc3ae44d85f7482bb8fe78edbcbd38f1
benjamin.cranwell <benzoscifo@yahoo.com>
2023-01-30 16:53:07 +0000

    run build

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,34 +1,15 @@
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=default" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-bg-image-js": () => import("./../../../src/pages/bg-image.js?export=default" /* webpackChunkName: "component---src-pages-bg-image-js" */),
-  "component---src-pages-case-studies-bbc-js": () => import("./../../../src/pages/caseStudies/bbc.js?export=default" /* webpackChunkName: "component---src-pages-case-studies-bbc-js" */),
-  "component---src-pages-case-studies-bloodhound-js": () => import("./../../../src/pages/caseStudies/bloodhound.js?export=default" /* webpackChunkName: "component---src-pages-case-studies-bloodhound-js" */),
-  "component---src-pages-case-studies-index-js": () => import("./../../../src/pages/caseStudies/index.js?export=default" /* webpackChunkName: "component---src-pages-case-studies-index-js" */),
-  "component---src-pages-case-studies-next-js": () => import("./../../../src/pages/caseStudies/next.js?export=default" /* webpackChunkName: "component---src-pages-case-studies-next-js" */),
-  "component---src-pages-case-studies-test-js": () => import("./../../../src/pages/caseStudies/test.js?export=default" /* webpackChunkName: "component---src-pages-case-studies-test-js" */),
-  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js?export=default" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-index-copy-js": () => import("./../../../src/pages/index copy.js?export=default" /* webpackChunkName: "component---src-pages-index-copy-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-sustainable-js": () => import("./../../../src/pages/sustainable.js?export=default" /* webpackChunkName: "component---src-pages-sustainable-js" */)
-}
-
-
-
-exports.head = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=head" /* webpackChunkName: "component---src-pages-404-jshead" */),
-  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=head" /* webpackChunkName: "component---src-pages-about-jshead" */),
-  "component---src-pages-bg-image-js": () => import("./../../../src/pages/bg-image.js?export=head" /* webpackChunkName: "component---src-pages-bg-image-jshead" */),
-  "component---src-pages-case-studies-bbc-js": () => import("./../../../src/pages/caseStudies/bbc.js?export=head" /* webpackChunkName: "component---src-pages-case-studies-bbc-jshead" */),
-  "component---src-pages-case-studies-bloodhound-js": () => import("./../../../src/pages/caseStudies/bloodhound.js?export=head" /* webpackChunkName: "component---src-pages-case-studies-bloodhound-jshead" */),
-  "component---src-pages-case-studies-index-js": () => import("./../../../src/pages/caseStudies/index.js?export=head" /* webpackChunkName: "component---src-pages-case-studies-index-jshead" */),
-  "component---src-pages-case-studies-next-js": () => import("./../../../src/pages/caseStudies/next.js?export=head" /* webpackChunkName: "component---src-pages-case-studies-next-jshead" */),
-  "component---src-pages-case-studies-test-js": () => import("./../../../src/pages/caseStudies/test.js?export=head" /* webpackChunkName: "component---src-pages-case-studies-test-jshead" */),
-  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js?export=head" /* webpackChunkName: "component---src-pages-contact-jshead" */),
-  "component---src-pages-index-copy-js": () => import("./../../../src/pages/index copy.js?export=head" /* webpackChunkName: "component---src-pages-index-copy-jshead" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=head" /* webpackChunkName: "component---src-pages-index-jshead" */),
-  "component---src-pages-sustainable-js": () => import("./../../../src/pages/sustainable.js?export=head" /* webpackChunkName: "component---src-pages-sustainable-jshead" */)
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-bg-image-js": () => import("./../../../src/pages/bg-image.js" /* webpackChunkName: "component---src-pages-bg-image-js" */),
+  "component---src-pages-case-studies-bbc-js": () => import("./../../../src/pages/caseStudies/bbc.js" /* webpackChunkName: "component---src-pages-case-studies-bbc-js" */),
+  "component---src-pages-case-studies-bloodhound-js": () => import("./../../../src/pages/caseStudies/bloodhound.js" /* webpackChunkName: "component---src-pages-case-studies-bloodhound-js" */),
+  "component---src-pages-case-studies-index-js": () => import("./../../../src/pages/caseStudies/index.js" /* webpackChunkName: "component---src-pages-case-studies-index-js" */),
+  "component---src-pages-case-studies-next-js": () => import("./../../../src/pages/caseStudies/next.js" /* webpackChunkName: "component---src-pages-case-studies-next-js" */),
+  "component---src-pages-case-studies-test-js": () => import("./../../../src/pages/caseStudies/test.js" /* webpackChunkName: "component---src-pages-case-studies-test-js" */),
+  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-copy-js": () => import("./../../../src/pages/index copy.js" /* webpackChunkName: "component---src-pages-index-copy-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-sustainable-js": () => import("./../../../src/pages/sustainable.js" /* webpackChunkName: "component---src-pages-sustainable-js" */)
 }
 
