012a1adb90c58aee3b4423ca7a570ba54ed45556
benjamin.cranwell <benzoscifo@yahoo.com>
2023-01-31 15:35:31 +0000

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react"
 import "../styles/global.css"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 import icon from '../images/kangaroo.svg'
 
 export default function Navbar() {
